2d03aa187b601cf404df9544c7f634da2c82703b
Deepak Mardi <deepakmardi22@gmail.com>
2024-02-27 16:38:57 +0530

    getting image

--- pages/api/captcha-image.jsx
+++ pages/api/captcha-image.jsx
@@ -1,1 +1,6 @@
-export default function handler(req, res) {}
+import * as fs from "fs";
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "image/png");
+  const imageBuffer = fs.readFileSync("./public/dogs-and-muffins/dog1.png");
+  res.send(imageBuffer);
+}
