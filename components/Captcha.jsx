8bdf64e3306db28246ab545d92b3ef72c8db14c1
Deepak Mardi <deepakmardi22@gmail.com>
2024-02-27 16:26:53 +0530

    captach layout

--- components/Captcha.jsx
+++ components/Captcha.jsx
@@ -1,8 +1,17 @@
 export default function Captcha() {
+  const imageLocations = new Array(9).fill(null).map((value, index) => {
+    return `/api/captcha-image?index=${index}.png`;
+  });
   return (
     <div className="captcha">
       <h2>Select all dogs:</h2>
-      <div className="captacha-images"></div>
+      <div className="captacha-images">
+        {imageLocations.map((imageUrl) => (
+          <div>
+            <img src={imageUrl} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
